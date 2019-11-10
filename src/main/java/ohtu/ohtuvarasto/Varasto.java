2871330a12e5841e850801a6f125f4a30d3fff0b
Laura S <46318664+sinikala@users.noreply.github.com>
2019-11-10 23:20:48 +0200

    codeclimate-testi loppu

--- src/main/java/ohtu/ohtuvarasto/Varasto.java
+++ src/main/java/ohtu/ohtuvarasto/Varasto.java
@@ -17,8 +17,7 @@ public class Varasto {
     }
 
     public Varasto(double tilavuus, double alkuSaldo) { // kuormitetaan
-        if (tilavuus > 0.0)
-        {
+        if (tilavuus > 0.0) {
             this.tilavuus = tilavuus;
         } else{ // virheellinen, nollataan
             this.tilavuus = 0.0;  // => käyttökelvoton varasto
